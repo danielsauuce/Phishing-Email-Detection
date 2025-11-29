fdd5ea97965bce2abb7af6f71d71074174c27732
Daniel <danieladeyd77@gmail.com>
2025-11-29 20:58:39 +0000

    federated learning privacy implemnted

--- src/privacy_preserving.py
+++ src/privacy_preserving.py
@@ -25,3 +25,26 @@ def add_differential_privacy_noise(X, epsilon=1.0):
     return X_noisy
 
 
+def simulate_federated_learning(X_train, y_train, n_clients=3):
+    """
+    Simulates federated learning:
+    - Split data across 'clients'
+    - Train separate models locally
+    - Aggregate predictions without sharing raw data
+    """
+    client_models = []
+    split_size = len(X_train) // n_clients
+    for i in range(n_clients):
+        start = i * split_size
+        end = (i + 1) * split_size if i < n_clients - 1 else len(X_train)
+        X_client = X_train.iloc[start:end]
+        y_client = y_train.iloc[start:end]
+
+        model = RandomForestClassifier(n_estimators=50, random_state=42)
+        model.fit(X_client, y_client)
+        client_models.append(model)
+        print(f"- Trained client {i+1} model on {len(X_client)} samples")
+
+    return client_models
+
+
